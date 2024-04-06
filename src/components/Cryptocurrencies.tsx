533deaf10b980267055956a228dee586ed60460d
TangangMcBride <tangang.mcbride@ictuniversity.edu.cm>
2024-04-06 18:32:08 +0100

    Added Cryptocurrencies

--- src/components/Cryptocurrencies.tsx
+++ src/components/Cryptocurrencies.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
+import { useGetCryptosQuery } from "../services/cryptoAPI";
+import { useState } from "react";
 
 function Cryptocurrencies() {
+  const { data, error, isLoading } = useGetCryptosQuery(10);
+  const [cryptos, setCryptos] = useState(data?.data?.coins);
+
+  if (isLoading) return <p>...Loading</p>;
+  if (error instanceof Error) return <p>{error.message}</p>;
+
+  console.log(cryptos);
+
   return (
-    <div>Cryptocurrencies</div>
-  )
+    <div>
+      <Row gutter={[32, 32]}>
+        {cryptos?.map((crypto: any) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={crypto.id}>
+            <Link to={`/crypto/${crypto.id}`}>
+              <Card
+                title={`${crypto.rank}. ${crypto.name}`}
+                extra={
+                  <img
+                    style={{
+                      width: "40px",
+                      height: "40px",
+                      // borderRadius: "50%",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                    src={crypto.iconUrl}
+                    alt={crypto.name}
+                  />
+                }
+                hoverable
+              >
+                <p>Price: {millify(crypto.price)}</p>
+                <p>Market Cap: {millify(crypto.marketCap)}</p>
+                <p>Daily Change: {millify(crypto.change)}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 }
 
-export default Cryptocurrencies
+export default Cryptocurrencies;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Row, Statistic, Typography,Button, Flex } from "antd";
+import { Col, Row, Statistic, Typography, Button, Flex } from "antd";
 import { useGetCryptosQuery } from "../services/cryptoAPI";
 import millify from "millify";
 import Cryptocurrencies from "./Cryptocurrencies";
@@ -8,16 +8,14 @@ import { Link } from "react-router-dom";
 const { Title } = Typography;
 
 const Home: React.FC = () => {
-
-   const { data, error, isLoading} = useGetCryptosQuery('yourArgumentHere');
-
+  const { data, error, isLoading } = useGetCryptosQuery("yourArgumentHere");
 
   if (isLoading) return <p>Loading...</p>;
   if (error instanceof Error) return <p>Error: {error.message}</p>;
 
   const globalStats = data?.data?.stats;
 
-console.log(data);
+  console.log(data);
 
   return (
     <div>
@@ -31,28 +29,69 @@ console.log(data);
       >
         GLOBAL CRYPTO STATS
       </Title>
-      <Row>
+
+      <Row justify="center" align="middle">
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
-          <Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} />
-          <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} />
+          <div style={{ textAlign: "center" }}>
+            <Statistic
+              title="Total Cryptocurrencies"
+              value={globalStats.total}
+            />
+          </div>
         </Col>
         <Col span={12}>
-          <Statistic title="Total marketCap" value={millify(globalStats.totalMarketCap)} />
-          <Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} />
+          <div style={{ textAlign: "center" }}>
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalStats.totalExchanges)}
+          />
+          </div>
         </Col>
       </Row>
+      <Row>
+        <Col span={12}>
+          <div style={{ textAlign: "center" }}>
+          <Statistic
+            title="Total Markets"
+            value={millify(globalStats.totalMarkets)}
+          />
+          </div>
+        </Col>
+
+        <Col span={12}>
+          <div style={{ textAlign: "center" }}>
+          <Statistic
+            title="Total marketCap"
+            value={millify(globalStats.totalMarketCap)}
+          />
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={12}>
+        <div style={{ textAlign: "center" }}>
+          <Statistic
+            title="Total 24h Volume"
+            value={millify(globalStats.total24hVolume)}
+          />
+          </div>
+        </Col>
+      </Row>
+
       <Flex justify="space-between" align="center">
         <Title level={2}>Top 10 Crypto Currencies in the World</Title>
-        <Button size= 'large' type="link"><Link to='/cryptocurrencies'>Show More</Link></Button>
+        <Button size="large" type="link">
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Button>
       </Flex>
-      <Cryptocurrencies/>
+      <Cryptocurrencies simplified />
       <Flex justify="space-between" align="center">
         <Title level={2}>Latest News on Cryptocurrencies</Title>
-        <Button size= 'large' href='/news' type="link"><Link to='/cryptocurrencies'>Show More</Link></Button>
+        <Button size="large" href="/news" type="link">
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Button>
       </Flex>
       <News />
-      
     </div>
   );
 };
